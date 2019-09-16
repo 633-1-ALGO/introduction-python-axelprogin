11b7570d1d2662849d203a2de6794a1493b47945
axelprogin <55376969+axelprogin@users.noreply.github.com>
2019-09-16 09:26:09 +0200

    sauvegarde 16.09.19

--- 8.1- Exercice - Séquence 1/sequence1.py
+++ 8.1- Exercice - Séquence 1/sequence1.py
@@ -4,3 +4,11 @@
 
 a = 11
 b = 42
+c = 0
+
+c = a
+a = b
+b = c
+
+print(a, "A")
+print(b, 'B')
--- 8.2- Exercice - Séquence 2/sequence2.py
+++ 8.2- Exercice - Séquence 2/sequence2.py
@@ -4,3 +4,7 @@
 
 nb_articles = 13
 prix_ht = 42.75
+tva = 7.7
+prix_tva = ((42.75 * 7.7) / 100 ) + 42.75
+
+print("Le prix TTC est de ", prix_tva * 13,' chf')
