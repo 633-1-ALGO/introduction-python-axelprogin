e95cfb1a5d1e978508a013ee9415cc3101d31223
axelprogin <55376969+axelprogin@users.noreply.github.com>
2019-09-16 09:45:51 +0200

    test

--- 9.1- Exercice - Traitement de string/string1.py
+++ 9.1- Exercice - Traitement de string/string1.py
@@ -1,2 +1,6 @@
 # Consigne : Récupérer le mot "chaine" du string s et l'afficher
 s = 'un exemple de chaine'
+
+if "chaine" in s:
+    print("chaine")
+
--- 9.2  Exercice - Traitement de string/string2.py
+++ 9.2  Exercice - Traitement de string/string2.py
@@ -1,3 +1,13 @@
 # Consigne : Rechercher le nombre d'occurences du mot "exemple" et l'afficher. Remplacer le mot "est" par "représente".
 # Bonus : Inverser le sens de lecture.
 texte = "Ceci est un exemple exemplaire d'exemple exempté d'exemple."
+
+txtSplit = texte.split("")
+cptExemple = 0
+
+for m in txtSplit:
+    reSplit = m.split("'")
+    print(reSplit)
+
+
+print(cptExemple)
